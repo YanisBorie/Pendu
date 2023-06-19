73e3878bcd5fb8755bf48b714d0b397e399db609
Yanis Borie <yanis.borie69@gmail.com>
2023-06-19 13:55:36 +0200

    Tableau vainqueur

--- final.php
+++ final.php
@@ -3,13 +3,13 @@
 
     if($_SESSION["erreur"] == 10) {
         echo "Le joueur " .$_SESSION["joueur2"]. " a perdu, victoire du joueur " .$_SESSION["joueur1"]. " ! Le mot était " .$_SESSION["mot"]. " !";
-        $ajout = array($_SESSION["joueur1"], $_SESSION["joueur2"], $_SESSION["mot"], 1, 0);
+        $ajout = array($_SESSION["joueur1"], $_SESSION["joueur2"], $_SESSION["mot"], 1, $_SESSION["nbcoups"]);
         $statement = $bdd->prepare("INSERT INTO partie (nom_joueur1, nom_joueur2, mot, victoire, nb_coup) VALUES (?, ?, ?, ?, ?);");
         $statement->execute($ajout);
         
     } else {
         echo "Le joueur " .$_SESSION["joueur1"]. " a perdu, victoire du joueur " .$_SESSION["joueur2"]. " avec " .$_SESSION["nbcoups"]. " coups ! Le mot était " .$_SESSION["mot"]. " !";
-        $ajout = array($_SESSION["joueur1"], $_SESSION["joueur2"], $_SESSION["mot"], 2, 0);
+        $ajout = array($_SESSION["joueur1"], $_SESSION["joueur2"], $_SESSION["mot"], 2, $_SESSION["nbcoups"]);
         $statement = $bdd->prepare("INSERT INTO partie (nom_joueur1, nom_joueur2, mot, victoire, nb_coup) VALUES (?, ?, ?, ?, ?);");
         $statement->execute($ajout);
     }
--- index.php
+++ index.php
@@ -11,6 +11,31 @@
 </head>
 <body>
     <p>djdjdjj</p>
+
+    <table>
+        <tr>
+            <th>Joueur Gagnant</th>
+            <th>Joueur Perdant</th>
+            <th>Mot</th>
+            <th>Nombre de coups</th>
+        </tr>
+    <?php 
+    $partieStatement = $bdd->prepare("SELECT * FROM partie");
+    $partieStatement->execute();
+    $parties = $partieStatement->fetchAll();
+
+    foreach($parties as $partie) { ?>
+        <tr>
+            <td><?php if($partie['victoire'] == 1) { echo $partie['nom_joueur1']; } else { echo $partie['nom_joueur2']; } ?></td>
+            <td><?php if($partie['victoire'] == 1) { echo $partie['nom_joueur2']; } else { echo $partie['nom_joueur1']; } ?></td>
+            <td><?php echo $partie['mot'] ?></td>
+            <td><?php echo $partie['nb_coup'] ?></td>
+        </tr>
+    <?php
+    }
+    ?>
+    </table>
+
     <button><a href="joueurs.php">Page Suivante</a></button>
 </body>
 </html>
